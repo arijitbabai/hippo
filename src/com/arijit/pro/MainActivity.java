b454c9c7fd32bf0a32f66b3008946310c6301a49
arijitbabai <babai9300@gmail.com>
2014-09-07 21:15:43 +0530

    network check

--- src/com/arijit/pro/MainActivity.java
+++ src/com/arijit/pro/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.view.*;
 import android.app.ActionBar;
+import android.net.*;
+import android.content.Context;
 
 public class MainActivity extends Activity 
 {
@@ -26,5 +28,21 @@ public class MainActivity extends Activity
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void checkNetwork(View view) {
+        String message;
+        /* Check the network connection */
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            message = "Connected! :D";
+        } else {
+            message = "Disconnected. :(";
+        }
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);   
     }
 }
